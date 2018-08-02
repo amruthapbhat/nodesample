929286f2e75d6a30aa119f978c2ead576b11f748
Pervody Bhat, Amrutha <amrutha.pervody.bhat@accenture.com>
2018-08-02 10:22:39 +0530

    Modified the data

--- outputs.tf
+++ outputs.tf
@@ -6,16 +6,8 @@ output "vpc_id" {
   value = "${aws_vpc.main.id}"
 }
 
-output "org0_ip" {
-  value = "${aws_instance.org.0.public_ip}"
-}
-
-output "org1_ip" {
-  value = "${aws_instance.org.1.public_ip}"
-}
-
-output "org2_ip" {
-  value = "${aws_instance.org.2.public_ip}"
+output "org_ip" {
+  value = "${aws_instance.org.*.public_ip}"
 }
 
 output "orderer_ip" {
